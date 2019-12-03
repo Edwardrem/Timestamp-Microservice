e7bf62d1bcc4e701f88270394d06772ec7e01e19
Glitch (steady-pincushion) <none>
2019-12-03 12:14:50 +0000

    💃🛢 Checkpoint ./server.js:5804786/3030 ./package.json:5804786/2046 ./views/index.html:5804786/11868

--- server.js
+++ server.js
@@ -1,32 +1,102 @@
-// server.js
-// where your node app starts
-
 // init project
 var express = require('express');
+var path = require('path');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
+//Use our public files
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res){
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//Returns true if the input is a number
+var isNumeric = function(input) {
+  return !isNaN(parseFloat(input)) && isFinite(input);
+}
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+//Returns true if the given month is a month
+var validMonth = function(month) {
+  switch(month.toLower())
+      {
+    case "january":
+      break;
+    case "february":
+      break;
+    case "march":
+      break;
+    case "april":
+      break;
+    case "may":
+      break;
+    case "june":
+      break;
+    case "july":
+      break;
+    case "august":
+      break;
+    case "september":
+      break;
+    case "october":
+      break;
+    case "november":
+      break;
+    case "december":
+      break;
+  }
+}
+
+//Returns true if the format is unix, false if natural, and undefined if neither
+var isUnixTimestamp = function(time)
+{
+  if(isNumeric(time))
+  {
+    return true;
+  }
+  //Check date?
+  return false;
+}
+
+//Given a date or unix time, returns an object with both
+var convertTime = function(time)
+{
+  var isUnix = isUnixTimestamp(time);
+  if(isUnix === undefined)
+    {
+      return {unix: null, natural: null};
+    }
+  if(isUnix)
+    {
+      var date = new Date(parseInt(time));
+      console.log(date);
+      return {unix: time, natural: trimDate(date.toDateString())};
+    }
+  return  {unix: Date.parse(time), natural: time};
+}
+
+//Trims the day off the date. ex: Tuesday January 5, 1999 --> January 5, 1999
+var trimDate = function(dateStr)
+{
+  var i = 0;
+  while(i < dateStr.length && dateStr[i] !== " ")
+    {
+      i++;
+    }
+  i++;
+  return dateStr.substring(i);
+}
+
+//Handle a specific request
+app.get("/:data", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+  //valid url request?
+  if(request.params && Object.keys(request.params).length > 0)
+    {
+      response.write(JSON.stringify(convertTime(request.params.data)));
+    }
+  response.end();
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
 });
